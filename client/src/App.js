cf2dd4cb03a42b22171b5a2e968773d905db4e1b
JoeyDaB3Ast <60456147+JoeyDaB3Ast@users.noreply.github.com>
2025-01-25 04:38:10 -0500

    added basic navbar functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,21 @@
-import React,{ useState, useEffect } from 'react'
+import Navbar from "./Navbar"
+import Calendar from "./pages/Calendar"
+import Chat from "./pages/Chat"
+import Entries from "./pages/Entries"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
   return (
-    <div>
-
-        {(typeof data.members === 'undefined') ? (
-          <p>Loading...</p>
-        ): (
-          data.members.map((member, i) => (
-            <p key={i}>{member}</p>
-          ))
-        )}
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/Entries" element={<Entries />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
