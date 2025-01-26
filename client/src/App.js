62cd5e22cf37fc11a4c4a9e129456b352040dd05
Adrian Yanovich Ghitis <adrianyanovich@gmail.com>
2025-01-26 03:11:56 -0500

    kiss

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Entries from "./pages/Entries"
 import ChatInterface from "./pages/Chat"; 
 import { Route, Routes } from "react-router-dom"
 import React from 'react';
+import EntryPage from "./pages/EntryPage.js";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/chat" element={<ChatInterface />} />
           <Route path="/calendar" element={<Calendar />} />
           <Route path="/Entries" element={<Entries />} />
+          <Route path="/entry/:id" element={<EntryPage />} />
         </Routes>
       </div>
     </>
